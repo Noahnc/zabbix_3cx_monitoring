82a75cf11543f370e88104f9e7c96ebf1b95baa4
Noahnc <nc.canadea@gmail.com>
2022-04-11 12:29:53 +0200

    added more comments

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@
 #
 #                    Version 1.0 | 04.04.2022
 
-
+# global variables
 zabbix_external_scripts_path="/usr/lib/zabbix/externalscripts/"
 project_folder_name="zabbix_3cx_monitoring"
 scrip_folder_path="$(dirname -- "$0")"
@@ -23,6 +23,7 @@ python_version="python3.9"
 
 trap ctrl_c INT
 
+# fuction to cancel the script
 function ctrl_c() {
     echo ""
     echo -e "\e[31mScript execution manually stopt.\e[39m"
@@ -33,10 +34,12 @@ function ctrl_c() {
     exit 1
 }
 
+# function to print a confirmation message
 function OK() {
     echo -e "\e[32m$1\e[39m"
 }
 
+# fuctions to print an error message and stop the script
 function error() {
     echo -e "\e[31m
 Error while executing the script:
@@ -45,23 +48,28 @@ Pleas check the shell output.\e[39m"
     exit 1
 }
 
+# check if script is run as root
 if (( $EUID != 0 )); then
     error "Pleas run this script with root privileges."
 fi
 
+# update apt cache
 apt update  || error "Error while updating the apt cache"
 
+# check if the zabbix folder exists on the system
 if ! [[ -d $zabbix_external_scripts_path ]]; then
     error "Zabbix script folder not found on this system, is the zabbix server installed?"
 fi
 
+# check if the python3.9 is installed and if not install it
 if ! [[ -x "$(command -v $python_version)" ]]; then
-    apt-get install $python_version -y
+    apt-get install $python_version -y || error "Error while installing python"
     OK "$python_version installed"
 else
     OK "$python_version already installed"
 fi
 
+# check if pip is installed and if not install it
 if ! [[ -x "$(command -v python3-pip)" ]]; then
     apt-get install python3-pip -y
     OK "pip installed"
@@ -69,10 +77,10 @@ else
     OK "pip already installed"
 fi
 
-
+# install python requirements
 python3.9 -m pip install -r "$scrip_folder_path/requirements.txt" || error "Error while installing python requirements"
 
-
+# check if script is allready installed and delete old files if so
 if ! [[ -d $api_models_folder_path ]]; then
     mkdir "$api_models_folder_path"
     OK "Folder for api models created"
@@ -82,9 +90,11 @@ else
     OK "Current Installation detected, removing old files"
 fi
 
-
+# copy files to the external script folder
 cp -r "$scrip_folder_path/src/$api_models_folder/." "$api_models_folder_path/" || error "Error while copying api models"
 cp -r "$scrip_folder_path/src/$addon_script_file" "$zabbix_external_scripts_path" || error "Error while copying api models"
+
+# make script executable
 chmod +x "${zabbix_external_scripts_path}${addon_script_file}" || error "Error while assigning execution permission to the script"
 
 OK "------------------------------------------------------"
--- src/monitoring_3cx.py
+++ src/monitoring_3cx.py
@@ -16,7 +16,7 @@ import socket
 import requests
 import sys
 
-# importing all models
+# importing api models
 from api_models_3cx_monitoring.trunks import trunks, welcome_from_dict_trunks
 from api_models_3cx_monitoring.services import service, welcome_from_dict_services
 from api_models_3cx_monitoring.system_status import status, welcome_from_dict_systemstatus
@@ -100,7 +100,7 @@ def main():
         scriptHealthCheck = True
         ScriptHealtCheck()
     else:
-        checkPort(domain, tcp_port) # check if port is open before trying to connect to 3cx api
+        checkPortAndExitOnError(domain, tcp_port) # check if port is open before trying to connect to 3cx api
         print(getJsonOfCategory(args.category))
 
 # function that calculates the percentage between two values
@@ -157,13 +157,15 @@ def getJsonOfCategory(category) -> str:
             }
             dic.append(temp_dic)
     else:
+            # exit script if no valid category is given
             exitScript(1, "Invalid category argument specified", category)
     try:
+        # parse dic to json and exit on error
         return json.dumps(dic, separators=(',', ':'), default=str)
     except Exception as e:
         exitScript(1, "error while creating json string", e)
 
-# function to get all 3cx services as services object
+# function to get all 3cx services as services object and return it
 def get3CXServices() -> service:
     response = getDataFrom3CXAPI('ServiceList')
     try:
@@ -172,7 +174,7 @@ def get3CXServices() -> service:
         exitScript(1, "3CX Services parse error", e)
     return data
 
-# function to get all 3cx trunks objects as list
+# function to get all 3cx trunks as list in an object and return it
 def get3CXTrunks() -> trunks:
     response = getDataFrom3CXAPI('TrunkList')
     try:
@@ -181,7 +183,7 @@ def get3CXTrunks() -> trunks:
         exitScript(1, "3CX Trunks parse error", e)
     return data
 
-# function to get 3cx status as object
+# function to get 3cx status as object and return it
 def get3CXSystemStatus() -> status:
     response = getDataFrom3CXAPI('SystemStatus')
     try:
@@ -192,13 +194,13 @@ def get3CXSystemStatus() -> status:
 
 # function that gets the data from the 3cx api on a specific recource url
 def getDataFrom3CXAPI(uri) -> str:
-    if auth_cookie is None: APIauthentication()
+    if auth_cookie is None: APIauthentication() # get auth cookie if not already available
     try:
         url = base_url_3cx + uri
         headers = {'content-type': 'application/json;charset=UTF-8'}
         response = session.get(url, headers=headers, cookies=auth_cookie)
     except Exception as e:
-        exitScript(1, "Error while connecting to 3cx api", e)
+        exitScript(1, "Error while connecting to 3cx api on recourse: " + uri, e)
     return response.text
 
 # function that gets the access cookie from 3cx api
@@ -213,25 +215,27 @@ def APIauthentication() -> None:
     except Exception as e:
         exitScript(1, "Error while connecting to 3cx server api", e)
     if response.status_code == 200 and response.text == 'AuthSuccess':
-        cookie = response.cookies
-        auth_cookie = cookie
+        auth_cookie = response.cookies
     else:
         exitScript(1, "API authentication error", response.text)
 
 # function to test all components of the script and return the status to the zabbix healthcheck item
 def ScriptHealtCheck() -> None:
-    # check if installed python version 
-    if sys.version_info < MIN_PYTHON_VERSION:
-        exitScript(1, "Python version is too old", sys.version_info)
-    checkPort(domain, tcp_port)
+    # check if port is open and exit with error if not
+    checkPortAndExitOnError(domain, tcp_port)
+
+    # get all data from 3cx api and parse to models
+    # if any error occurs, the script will be terminated with basic healthcheck error message, which is shown in the zabbix healthcheck item
     testjson1 = getJsonOfCategory("3cx-status")
     testjson2 = getJsonOfCategory("3cx-info")
     testjson3 = getJsonOfCategory("3cx-services")
     testjson4 = getJsonOfCategory("3cx-trunks")
+
+    # if no exception occurred, the script will be terminated with healthcheck OK
     exitScript(0, "OK", "Script test successful, everything works fine")
 
-# checks if the specified port is open on the remote host
-def checkPort(host: str, port: int) -> None:
+# checks if the specified port is open on the remote host and exits the script if not
+def checkPortAndExitOnError(host: str, port: int) -> None:
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.settimeout(3)
@@ -242,7 +246,8 @@ def checkPort(host: str, port: int) -> None:
         exitScript(1, "Can't connect to " + host + " on Port " + str(port), e)
 
 # function to exit the script with a specific exit code and message
-# errors are only printed, if the script is executed in debug mode as healthcheck
+# errors are only printed, if the script is executed in debug mode or as healthcheck
+# healthcheck only prints basic information, while debug mode prints all information
 def exitScript(exitCode: int, message: str, info) -> None:
     if scriptHealthCheck and debugMode == False: print(message)
     if debugMode:
@@ -250,5 +255,6 @@ def exitScript(exitCode: int, message: str, info) -> None:
         print(info)
     exit(exitCode)
 
+# call main function if script is executed as main
 if __name__ == '__main__':
     main()
